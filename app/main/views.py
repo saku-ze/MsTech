27650502c7e1089bcd6a944dfb5cf81600972d68
Sakura <838683046@qq.com>
2020-11-12 19:57:47 +0800

    maybe okay

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,18 @@
+from flask import render_template
 from . import bp
 
 
 @bp.route("/")
 @bp.route("/index")
 def index():
-    return "Hi, Nice to meet you"
+    return render_template('index.html')
+
+
+@bp.route("/members")
+def members():
+    return render_template('members.html')
+
+
+@bp.route("/project/<int:id>")
+def show_project(id):
+    return render_template("wedo2.html")
